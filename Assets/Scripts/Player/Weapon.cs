5c095bddbc1bd1f120e386a6dd8ccaa06414f2ac
tomasferencik <tomife2@gmail.com>
2024-11-21 10:17:28 +0100

    fixing shooting every 0.8 sec

--- Assets/Scripts/Player/Weapon.cs
+++ Assets/Scripts/Player/Weapon.cs
@@ -4,15 +4,23 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] private Transform firePoint;
     [SerializeField] private GameObject bulletPrefab;
+    private float timeBetweenShots = 0.8f; 
+    private float timeSinceLastShot = 0f;
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.J))
+
+        if (Input.GetKeyDown(KeyCode.J) && timeSinceLastShot >= timeBetweenShots)
         {
             Shoot();
+            timeSinceLastShot = 0f;
         }
+        
+        timeSinceLastShot += Time.deltaTime;
     }
+
     private void Shoot()
     {
-        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation); 
     }
 }
